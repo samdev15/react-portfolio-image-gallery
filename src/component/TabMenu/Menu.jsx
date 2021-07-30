53d066e09072b929786c7925194d0ed8a8f9c855
samdev15 <pawan.gupta@beyondkey.com>
2021-07-30 17:19:36 +0530

    updated file

---
+++ src/component/TabMenu/Menu.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useState } from 'react';
+import '../TabMenu/menu.css';
+import MenuData from './MenuData';
+
+function Menu() {
+
+    const [menuItem, setMenuItem] = useState(MenuData);
+
+    const filterItem = (item) => {
+        const updateItem = MenuData.filter((ele) => {
+            return (item === ele.category);
+        });
+
+        setMenuItem(updateItem);
+    }
+
+    return (
+        <div>
+            <h1 className="text-center my-3">Order Your Favourite Dish</h1>
+            <hr />
+            <div className="container d-flex justify-content-around">
+                <div className="menu-item">
+                    <button className="btn btn-md btn-warning" onClick={() => filterItem('morning')}>Morning</button>
+                </div>
+                <div className="menu-item">
+                    <button className="btn btn-md btn-warning" onClick={() => filterItem('snacks')}>Snacks</button>
+                </div>
+                <div className="menu-item">
+                    <button className="btn btn-md btn-warning" onClick={() => filterItem('lunch')}>Lunch</button>
+                </div>
+                <div className="menu-item">
+                    <button className="btn btn-md btn-warning" onClick={() => filterItem('evening')}>Evening</button>
+                </div>
+                <div className="menu-item">
+                    <button className="btn btn-md btn-warning" onClick={() => filterItem('dinner')}>Dinner</button>
+                </div>
+                <div className="menu-item">
+                    <button className="btn btn-md btn-warning" onClick={() => setMenuItem(MenuData)}>All</button>
+                </div>
+            </div>
+
+            <div className=" container-fluid mt-5 my-5">
+                <div className="row">
+                    <div className="col-10 mx-auto mt-5">
+                        <div className="row gy-5  container-fluid">
+                            {
+                                menuItem.map((elem) => {
+                                    return (
+                                        <div className="col-4" key={elem.id}>
+                                            <div className="row bk-card">
+                                                <div className="col-4">
+                                                    <img src={elem.image} alt={elem.title} width="150px" height="150px" />
+                                                </div>
+                                                <div className="card-body col-8">
+                                                    <h5 className="card-title">{elem.title}</h5>
+                                                    <p className="card-text">{elem.description}</p>
+                                                    <div className="row">
+                                                        <div className="col-6 price">{elem.price}</div>
+                                                        <div className="col-6">
+                                                            <button className="btn btn-sm btn-custom">Order Now</button>
+                                                        </div>
+                                                    </div>
+                                                    <br />
+                                                    <p>*Prices may very as selected date.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Menu
---
+++ src/component/TabMenu/MenuData.jsx
@@ -0,0 +1,84 @@
+const Data = [
+    {
+        "id": "2514eb19-708e-429d-a637-00e9ef48f7af",
+        "title": "Otar Taktakishvili MORNING",
+        "price": "20₹",
+        "description": "Wulfhere survived the defeat but evidently lost some degree of control.",
+        "image": "https://picsum.photos/200/308",
+        "category": "morning"
+    },
+    {
+        "id": "0dc860f5-f62f-4622-b9ef-e1d903fcbcf9",
+        "title": "Marc Shaiman LUNCH",
+        "price": "50₹",
+        "description": "His accession marked the end of Oswiu of Northumbria's overlordship.",
+        "image": "https://picsum.photos/200/309",
+        "category": "lunch"
+    },
+    {
+        "id": "51716b3b-6f88-431a-815c-a39f45110c9d",
+        "title": "David Wark DINNER",
+        "price": "100₹",
+        "description": "William Harrison Ainsworth, a Victorian novelist considered.",
+        "image": "https://picsum.photos/200/300",
+        "category": "dinner"
+    },
+    {
+        "id": "c34e8d37-d6b2-470f-b585-3e6dfa1f4317",
+        "title": "Annette Funicello EVENING",
+        "price": "150₹",
+        "description": "At 22:00, the German forces launched a severe counter-attack against.",
+        "image": "https://picsum.photos/200/301",
+        "category": "evening"
+    },
+    {
+        "id": "c803839e-4de4-4eab-86d6-47665f98d14f",
+        "title": "Johann Reinhold SNACKS",
+        "price": "120₹",
+        "description": "Boyd went to the sites of the Battle of Fish Creek and the Battle.",
+        "image": "https://picsum.photos/200/302",
+        "category": "snacks"
+    },
+    {
+        "id": "cdb042eb-5a68-4a79-89f3-64149820c4fc",
+        "title": "Giosuè Carducci EVENING",
+        "price": "80₹",
+        "description": "Approximately 74 percent of the people living in Metro Vancouver live.",
+        "image": "https://picsum.photos/201/302",
+        "category": "evening"
+    },
+    {
+        "id": "eff60c47-5c15-4950-85fa-ebecff9f3ee2",
+        "title": "Enzo Scifo MORNING",
+        "price": "110₹",
+        "description": "It may therefore be significant that Potts dedicated.",
+        "image": "https://picsum.photos/200/304",
+        "category": "morning"
+    },
+    {
+        "id": "685ba2f9-5861-4311-a5f2-b149f005276a",
+        "title": "Joaquim Chissano MORNING",
+        "price": "12₹",
+        "description": "Since the sovereignty change, the association encourages Scouts.",
+        "image": "https://picsum.photos/200/305",
+        "category": "morning"
+    },
+    {
+        "id": "305c7c09-d6a1-4ffa-a5d8-6001365fdf09",
+        "title": "Owais Shah DINNER",
+        "price": "35₹",
+        "description": "Like the herpestids, it has no prescrotal glands.",
+        "image": "https://picsum.photos/200/306",
+        "category": "dinner"
+    },
+    {
+        "id": "51156440-8abe-43f9-9376-aa95ee554951",
+        "title": "Dražen Petrović EVENING",
+        "price": "25₹",
+        "description": "Internationally, her work is held in the Aboriginal Art Museum.",
+        "image": "https://picsum.photos/200/307",
+        "category": "evening"
+    }
+];
+
+export default Data;
